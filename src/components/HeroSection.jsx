61f11287741013588399fa8b339a3fdc9d20bf36
gulnisademir <gulnisademir@hotmail.com>
2024-04-03 21:15:31 +0300

    added slider

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Slider from "react-slick";
 
-function HeroSection() {
+export default function SimpleSlider() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div>HeroSection</div>
-  )
+    <div className='container mx-auto'>
+    <Slider {...settings}>
+      <div>
+        <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-1.jpg" />
+        <div>
+        <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-2.jpg" />
+        </div>
+        <div>
+          <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg" alt="" />
+        </div>
+      </div>
+    </Slider>
+     </div>
+  );
 }
 
-export default HeroSection
