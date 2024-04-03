a530903156deb93185ab2b5f7c96a2021cba0402
gulnisademir <gulnisademir@hotmail.com>
2024-04-03 21:57:46 +0300

    update slider

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 import Slider from "react-slick";
 
-export default function SimpleSlider() {
-  var settings = {
+export const HeroSection = () => {
+  const settings = {
     dots: false,
     infinite: true,
     speed: 500,
+    arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-  };
+  }
   return (
-    <div className='container mx-auto'>
-    <Slider {...settings}>
-      <div>
-        <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-1.jpg" />
+    <div className="container mx-auto">
+      <Slider {...settings}>
         <div>
-        <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-2.jpg" />
+          <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-1.jpg" />
         </div>
         <div>
-          <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg" alt="" />
+          <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-2.jpg" />
         </div>
-      </div>
-    </Slider>
-     </div>
-  );
+        <div>
+          <img src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg" />
+        </div>
+      </Slider>
+    </div>
+  )
 }
-
